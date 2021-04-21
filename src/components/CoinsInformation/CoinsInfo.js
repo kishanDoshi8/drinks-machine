33193a0328448ea96bf50d74df76fe8861f65cca
Kishan Doshi <kishan.doshi88@gmail.com>
2021-04-21 19:56:10 -0400

    coins info layout and styling

--- src/components/CoinsInformation/CoinsInfo.js
+++ src/components/CoinsInformation/CoinsInfo.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 export default function CoinsInfo({coins}) {
 
+    const [totalAmount, setTotalAmount] = useState(0);
+
+    useEffect(() => {
+        let total = 0;
+        coins.forEach(coin => {
+            total += coin.amount * coin.quantity;
+        })
+        setTotalAmount(total);
+    }, [coins]);
+
     return (
-        <div>
-            {coins.map((coin) => (
-                <div key={coin.amount}>
-                    <h1>{coin.name}</h1>
-                    <h4>{coin.quantity}</h4>
+        <>
+            <h1 className="section-title">You have</h1>
+            <div className="coins-info">
+                {coins.map((coin) => (
+                    <div key={coin.amount} className="coin-info">
+                        <h4 className="coin-name">{coin.name}</h4>
+                        <p className="coin-quantity">{coin.quantity}</p>
+                        <p className="coin-total"> {coin.amount * coin.quantity} <i className="fas fa-dollar-sign"></i></p>
+                    </div>
+                ))}
+                <div className="coin-info">
+                    <h4 className="coin-name">Total</h4>
+                    <p className="coin-quantity">{totalAmount} <i className="fas fa-dollar-sign"></i></p>
                 </div>
-            ))}
-        </div>
+            </div>
+        </>
     )
 }
